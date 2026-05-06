12c17663bc564f855abd7d3f07a81c19bc78b9d1
quang <nguyenthequang080203@gmail.com>
2026-05-07 00:07:24 +0700

    edit mail back to q

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
 
   const { error } = await resend.emails.send({
     from: "Bench <onboarding@resend.dev>",
-    to: process.env.CONTACT_EMAIL ?? "trungtran3423@gmail.com",
+    to: process.env.CONTACT_EMAIL ?? "quanganh2k3@gmail.com",
     subject: "Có người trả lời Bench!!!",
     text: message,
   });
